d0a99e1c2ae3bfe210868c7f53136e52da4fc029
Javier Ramirez <apollo.infinity88@gmail.com>
2018-04-07 23:58:34 -0700

    created a private object then returned inside the IIFE to make it accesible to ther IIFE functions.

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
 // Budget Controller
-var budgetController = (function() {
-  
+var budgetController = (function () {
+
 
 })();
 
 // UI Controller
-var UIController = (function() {
+var UIController = (function () {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    }
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstrings: function(){
+            
+        }
+    };
 
 })();
 
 // Global App Controller
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var ctrlAddItem = function(){
-        console.log('It works');
+    var ctrlAddItem = function () {
+        // Gets the field input data.
+        var input = UICtrl.getInput();
+        console.log(input);
     }
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keypress', function(event){
-        if(event.keycode === 13 || event.which == 13) {
+    document.addEventListener('keypress', function (event) {
+        if (event.keycode === 13 || event.which == 13) {
             ctrlAddItem();
         }
     });
 
+
 })(budgetController, UIController);
-
-
-
-
