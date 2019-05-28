9fffe4ee9990a72293b78b4aedc93f3b5fcd2d75
Javier Ramirez <apollo.infinity88@gmail.com>
2019-05-28 10:27:51 -0700

    Added JS to show new items added to the front-end

--- app.js
+++ app.js
@@ -63,7 +63,9 @@ const UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: ".add__btn"
+        inputBtn: ".add__btn",
+        incomeContainer: ".income__list",
+        expensesContainer: ".expenses__list"
     }
 
     return {
@@ -76,6 +78,37 @@ const UIController = (function () {
 
         },
         addListItem: function (obj, type) {
+            let html, element;
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = `<div class="item clearfix" id="income-${obj.id}">
+                <div class="item__description">${obj.description}</div>
+                <div class="right clearfix">
+                    <div class="item__value">${obj.value}</div>
+                    <div class="item__delete">
+                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                </div>
+            </div>`;
+                document.querySelector(element).insertAdjacentHTML('beforeend', html);
+
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = `<div class="item clearfix" id="expense-${obj.id}">
+                <div class="item__description">${obj.description}</div>
+                <div class="right clearfix">
+                    <div class="item__value">${obj.value}</div>
+                    <div class="item__percentage">21%</div>
+                    <div class="item__delete">
+                        <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
+                    </div>
+                </div>
+            </div>`;
+                document.querySelector(element).insertAdjacentHTML('beforeend', html);
+            }
+            // Replace the placeholder text with actal data
+            // Used ES6 template literal for this part of the tut
 
         },
         getDOMstrings: function () { return DOMstrings }
@@ -107,6 +140,7 @@ const controller = (function (budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
